7ada0061b6ec9abdb65d4d1f9bc4f46c80c12c4c
eralpkor <eralp.ny@gmail.com>
2019-11-19 14:17:09 -0500

    added listing file

---
+++ database/migrations/20191119105029_listings.js
@@ -0,0 +1,29 @@
+exports.up = function (knex) {
+  return knex.schema.createTable(
+    'listings', tbl => {
+      tbl
+        .integer("user_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+        .notNullable();
+      tbl.increments();
+      tbl.string("zipcode", 128);
+      tbl.string("address", 255);
+      tbl.integer("price");
+      tbl.string("phone");
+      tbl.string("start_date");
+      tbl.string("end_date");
+      tbl.string("description", 500);
+      tbl.boolean("rented");
+      tbl.string("img")
+      tbl.timestamps(true, true);
+    }
+  )
+
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('listings')
+};
