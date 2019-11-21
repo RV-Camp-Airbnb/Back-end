770af8e1f000421fe162e5bc74f5e48aa612aa42
eralpkor <eralp.ny@gmail.com>
2019-11-21 12:12:56 -0500

    added listings

--- auth/auth-rv-router.js
+++ auth/auth-rv-router.js
@@ -11,35 +11,49 @@ const restricted = require('../auth/restricted-middleware');
 
 router.post('/register', (req, res) => {
 
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 8);
-    user.password = hash;
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
 
-    Users.addUsers(user)
-        .then(addedUser => {
-            res.status(201).json(addedUser);
-        })
-        .catch(err => {
-          console.log(err)
-            res.status(500).json({ message: 'Error registering, Try again' });
-        })
+  Users.addUsers(user)
+    .then(addedUser => {
+      res.status(201).json(addedUser);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        message: 'Error registering, Try again'
+      });
+    })
 })
 
 router.post('/login', (req, res) => {
-    const { username, password } = req.body;
+  const {
+    username,
+    password
+  } = req.body;
 
-    Users.findByUser({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                const token = getToken(user);
-                res.status(200).json({ message: 'Logged In', token });
-            } else {
-                res.status(401).json({ message: 'Invalid Credentials' });
-            };
-        }).catch(err => {
-            res.status(500).json({ message: 'Error logging in, Try again' });
-        })
+  Users.findByUser({
+      username
+    })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getToken(user);
+        res.status(200).json({
+          message: 'Logged In',
+          token
+        });
+      } else {
+        res.status(401).json({
+          message: 'Invalid Credentials'
+        });
+      };
+    }).catch(err => {
+      res.status(500).json({
+        message: 'Error logging in, Try again'
+      });
+    })
 });
 
 // added by me
@@ -51,20 +65,22 @@ router.get('/users', restricted, (req, res) => {
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ message: 'Error getting users...' });
+      res.status(500).json({
+        message: 'Error getting users...'
+      });
     });
 });
 
 function getToken(user) {
-    const payload = {
-        subject: user.id,
-        username: user.username,
-        // jwtid: user.id
-    };
-    const options = {
-        expiresIn: '2h',
-    };
-    return jwt.sign(payload, secret.jwtSecret, options);
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    // jwtid: user.id
+  };
+  const options = {
+    expiresIn: '2h',
+  };
+  return jwt.sign(payload, secret.jwtSecret, options);
 }
 
 module.exports = router;
